2999407da8e7d05ab1d58c1a530e682ecdfe038e
Nova <monarch3808@gmail.com>
2025-03-25 18:10:37 +0100

    add: test navbar reactive to the user login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
-	import '../app.css';
+	import { page } from '$app/state'
+	import '../app.css'
 
-	let { children } = $props();
+	let { children } = $props()
 </script>
 
-{@render children()}
+<svelte:head>
+	<title>SvelteKit Auth</title>
+</svelte:head>
+
+<nav>
+	{#if !$page.data.user}
+		<a href="/login">Login</a>
+		<a href="/register">Register</a>
+	{/if}
+
+	{#if $page.data.user}
+		<a href="/admin">Admin</a>
+
+		<form action="/logout" method="POST">
+			<button type="submit">Log out</button>
+		</form>
+	{/if}
+</nav>
+
+<main>
+	{@render children?.()}
+</main>
