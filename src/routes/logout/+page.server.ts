d899925d48cd08b2e375d59fa0f87fb95356b5c0
Nova <monarch3808@gmail.com>
2025-03-25 17:34:47 +0100

    create: logout API

---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,18 @@
+import { type Actions, redirect } from '@sveltejs/kit';
+
+export const load = async () => {
+	redirect(302, '/')
+}
+
+export const actions: Actions = {
+	default({ cookies }) {
+		// eat the cookie
+		cookies.set('session', '', {
+			path: '/',
+			expires: new Date(0),
+		})
+
+		// redirect the user
+		redirect(302, '/login')
+	},
+}
