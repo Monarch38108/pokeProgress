38d1fdb358722e31ca58c08bab4ac7082117a72f
Nova <monarch3808@gmail.com>
2025-03-26 13:08:04 +0100

    add: created a Tracker List component

---
+++ src/lib/components/TrackerList.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { TrackerData } from '../../interfaces/TrackerData';
+
+	export let trackerData: TrackerData[]
+</script>
+
+{#if trackerData.length < 1 }
+	<h1>You are not tracking any progress of the games yet.</h1>
+{/if}
+
+{#each trackerData as tracker}
+	<p>Tracker: {tracker.id} for the game {tracker.game.title}</p>
+	<p>Progress: {tracker.progress}%</p>
+{/each}
