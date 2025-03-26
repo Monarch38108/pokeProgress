603dbade8a9b9dc2f3067616b64e975f17b6ee21
Nova <monarch3808@gmail.com>
2025-03-26 13:20:18 +0100

    add: keys for svelte

--- src/lib/components/TrackerList.svelte
+++ src/lib/components/TrackerList.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
 	import type { TrackerData } from '../../interfaces/TrackerData';
-
-	export let trackerData: TrackerData[]
+	export let trackerData: TrackerData[];
 </script>
 
-{#if trackerData.length < 1 }
+{#if trackerData.length < 1}
 	<h1>You are not tracking any progress of the games yet.</h1>
 {/if}
 
-{#each trackerData as tracker, index}
+{#each trackerData as tracker, index (tracker.id || index)}
 	<p>Tracker: {index} for the game {tracker.game.title}</p>
 	<p>Progress: {tracker.progress}%</p>
 {/each}
