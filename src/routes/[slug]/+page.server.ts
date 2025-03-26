e30243d75a10df8d7ad8666fcc294e6cf0ff4481
Nova <monarch3808@gmail.com>
2025-03-26 13:08:58 +0100

    change: requesting trackers from the database

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,12 +1,42 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import type { TrackerData } from '../../interfaces/TrackerData';
+import { prisma } from '$lib/prisma';
+
+let trackers: TrackerData[];
+
 
 export const load: PageServerLoad = async ({ locals, params }) => {
 	// redirect user if not logged in
 	if (!locals.user) {
-		redirect(302, '/')
+		redirect(302, '/');
+	}
+
+	const user = await prisma.user.findUnique({
+		where: {
+			username: params.slug
+		}
+	});
+	if (user) {
+		trackers = await prisma.tracker.findMany({
+			where: { userId: user.id },
+			select: {
+				id: true,
+				progress: true,
+				isPublic: true,
+				createdAt: true,
+				updatedAt: true,
+				game: {
+					select: {
+						id: true,
+						title: true,
+					}
+				}
+			}
+		});
 	}
 	return {
-		pageOwner:  params.slug,
-	}
-}
+		pageOwner: params.slug,
+		trackerData: trackers
+	};
+};
