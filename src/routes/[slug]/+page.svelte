9cb7f5f21fef4df5bf9cb787a723cf1b646d3eb1
Nova <monarch3808@gmail.com>
2025-03-26 09:48:52 +0100

    add: example frontend for testing loading-data

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { page } from '$app/state';
+export let data: {
+	pageOwner: string,
+}
+</script>
+
+{#if data.pageOwner === page.data.user.name}
+	<h1>Here are your trackers!</h1>
+	{:else }
+	<h1>Trackers of {data.pageOwner}!</h1>
+{/if}
