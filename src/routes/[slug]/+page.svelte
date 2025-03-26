fcd67a54a0b5b29f420a76573a3a1f817053883c
Nova <monarch3808@gmail.com>
2025-03-26 13:08:38 +0100

    change: displaying requested trackers

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
+import TrackerList from '$lib/components/TrackerList.svelte';
+import type { TrackerData } from '../../interfaces/TrackerData';
 import { page } from '$app/state';
 export let data: {
 	pageOwner: string,
+	trackerData : TrackerData[]
 }
 </script>
-
-{#if data.pageOwner === page.data.user.name}
-	<h1>Here are your trackers!</h1>
-	{:else }
-	<h1>Trackers of {data.pageOwner}!</h1>
+{#if data.pageOwner === page.params.slug}
+	<h1>Your trackers:</h1>
+	<TrackerList trackerData="{data.trackerData}"/>
 {/if}
