49280062e7705474d3c0206d13e8b7ac1bf46627
Su5upern0va <monarch38108@gmail.com>
2025-06-13 08:20:33 +0200

    feat(admin) fetching userdata and passing it to the component

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,8 +1,24 @@
-import { redirect } from '@sveltejs/kit';
+import {redirect} from '@sveltejs/kit';
+import {prisma} from "$lib/prisma";
+import type {UserData} from "../../interfaces/UserData";
 
 export const load = async ({ locals }) => {
 	// redirect user if logged in
 	if (locals.user.role !== 'ADMIN') {
 		redirect(302, '/')
+	}
+
+	const users  = await prisma.user.findMany({
+		select: {
+			username: true,
+			role: true
+		}
+	});
+
+	const games = "games"
+
+	return {
+		users: users,
+		games: games,
 	}
 }
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,5 +1,18 @@
+<script lang="ts">
+    import GameAdministration from "$lib/components/GameAdministration.svelte";
+    import UserAdministration from "$lib/components/UserAdministration.svelte";
+    import type {UserData} from "../../interfaces/UserData";
+    import type {Game} from "@prisma/client";
+
+    export let data: {
+        users : UserData[];
+        games : Game[];
+    }
+    console.log(data);
+</script>
+
 <h1>Admin Page</h1>
 <h2>User-Administration</h2>
-<userAdministration></userAdministration>
+<UserAdministration UserData="{data.users}"></UserAdministration>
 <h2>game-Administration</h2>
 <gameAdministration></gameAdministration>
