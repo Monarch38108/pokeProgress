fecf6e9a70c14c79c277b9589dda8b03a5fe56ea
kapitz01 <tim.kapitz@ptb.de>
2025-06-13 11:27:44 +0200

    fix(userdashboard) using wrong variable name for slug

--- src/routes/[userdashboard]/+page.svelte
+++ src/routes/[userdashboard]/+page.svelte
@@ -11,17 +11,17 @@
 </script>
 
 <div class="container mx-auto px-4 py-8">
-	{#if data.pageOwner && data.pageOwner === page.params.slug}
+	{#if data.pageOwner && data.pageOwner === page.params.userdashboard}
 		<div class="mb-8 flex items-center">
 			<UserIcon size={32} class="mr-4 text-red-500" />
 			<h1 class="text-3xl font-bold text-gray-800">Your Trackers</h1>
 		</div>
 		<TrackerList trackerData={data.trackerData} />
-	{:else if data.pageOwner && data.pageOwner !== page.params.slug}
+	{:else if data.pageOwner && data.pageOwner !== page.params.userdashboard}
 		<div class="mb-8 flex items-center">
 			<EyeIcon size={32} class="mr-4 text-blue-500" />
 			<h1 class="text-3xl font-bold text-gray-800">
-				Trackers of {page.params.slug}
+				Trackers of {page.params.userdashboard}
 			</h1>
 		</div>
 		<TrackerList trackerData={data.trackerData} />
@@ -29,7 +29,7 @@
 		<div class="mb-8 flex items-center">
 			<EyeIcon size={32} class="mr-4 text-blue-500" />
 			<h1 class="text-3xl font-bold text-gray-800">
-				The user {page.params.slug} was not found!
+				The user {page.params.userdashboard} was not found!
 			</h1>
 		</div>
 	{/if}
